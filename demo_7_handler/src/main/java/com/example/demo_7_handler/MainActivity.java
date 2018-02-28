c0925b3dc295914d263b5b51c402eba51fa6cb0a
happyjiatai <835353929@qq.com>
2018-02-28 20:45:43 +0800

    demo_7_handler fix handler memoryleaked

--- demo_7_handler/src/main/java/com/example/demo_7_handler/MainActivity.java
+++ demo_7_handler/src/main/java/com/example/demo_7_handler/MainActivity.java
@@ -6,13 +6,15 @@ import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.lang.ref.WeakReference;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Handler handler = new MyHandler();
+        final MyHandler handler = new MyHandler(this);
         new Thread(){
             @Override
             public void run() {
@@ -23,9 +25,19 @@ public class MainActivity extends AppCompatActivity {
         }.start();
     }
 
-    class MyHandler extends Handler{
+    private static class MyHandler extends Handler{
+        private WeakReference<MainActivity> mActivity;
+
+        public  MyHandler(MainActivity activity){
+            mActivity = new WeakReference<MainActivity>(activity);
+        }
+
         @Override
         public void handleMessage(Message msg) {
+            MainActivity mainActivity = mActivity.get();
+            if (mainActivity == null) {
+                return;
+            }
             super.handleMessage(msg);
         }
     }
