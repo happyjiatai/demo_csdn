7864eaca87e2b0578cdeb40ba9ba220de6f7e832
happyjiatai <835353929@qq.com>
2018-03-04 10:10:40 +0800

    demo_8_mat add demo for mat

--- demo_7_handler/src/main/java/com/example/demo_7_handler/MainActivity.java
+++ demo_7_handler/src/main/java/com/example/demo_7_handler/MainActivity.java
@@ -40,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         private WeakReference<MainActivity> mActivity;
 
         public  MyThread(MainActivity activity){
+            super("zjtThread");
             mActivity = new WeakReference<>(activity);
         }
 
@@ -47,10 +48,10 @@ public class MainActivity extends AppCompatActivity {
         public void run() {
             MainActivity mainActivity = mActivity.get();
             if (mainActivity == null) {
-                return;
+               return;
             }
             super.run();
-            for(int i = 0 ; i < 50 ; i ++){
+            for(int i = 0 ; i < 500 ; i ++){
                 Log.d("zjt", "i = " + i);
                 if(mainActivity == null){
                     Log.d("zjt", "the weakreference is null");
@@ -59,10 +60,17 @@ public class MainActivity extends AppCompatActivity {
                 SystemClock.sleep(2000);
             }
 
-            Looper.prepare();
+            /*Looper.prepare();
             MyHandler handler = new MyHandler(mainActivity);
             handler.sendEmptyMessage(0);
-            Looper.loop();
+            Looper.loop();*/
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("zjt", "onDestory");
+        System.gc();
     }
 }
---
+++ demo_8_mat/src/main/java/com/example/demo_8_mat/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.demo_8_mat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new matThread().start();
+    }
+
+    class matThread extends Thread{
+        @Override
+        public void run() {
+            super.run();
+            try {
+                Thread.sleep(30000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
