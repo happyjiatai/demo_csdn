2b2d8f7bc7558d53bfa150d74565f24ada105b37
happyjiatai <835353929@qq.com>
2018-03-08 20:45:34 +0800

    demo_9_futureTask add Thread+FutureTask

---
+++ demo_9_futuretask/src/main/java/com/example/demo_9_futuretask/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.demo_9_futuretask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "FutureTaskTest";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        method1();
+    }
+
+    private static void method1() {
+        FutureTask<Integer> futureTask = new FutureTask<Integer>(new MyCallable());
+        new Thread(futureTask).start();
+        try {
+            int sum = futureTask.get();
+            Log.d(TAG, "the sum is :" + sum);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class MyCallable implements Callable<Integer>{
+
+        @Override
+        public Integer call() throws Exception {
+            int sum = 0;
+            for(int i = 0; i < 10; i++){
+                Log.d(TAG, "the i is : " + i);
+                sum += i;
+                Thread.sleep(200);
+            }
+            return sum;
+        }
+    }
+}
