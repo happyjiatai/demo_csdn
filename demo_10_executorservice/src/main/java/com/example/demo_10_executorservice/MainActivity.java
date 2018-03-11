29b277072d4d9eeb15ac04301f05d50e14c79920
happyjiatai <835353929@qq.com>
2018-03-11 11:09:21 +0800

    demo_10_ExecutorService submit 1 2 3 demo

--- demo_10_executorservice/src/main/java/com/example/demo_10_executorservice/MainActivity.java
+++ demo_10_executorservice/src/main/java/com/example/demo_10_executorservice/MainActivity.java
@@ -2,12 +2,74 @@ package com.example.demo_10_executorservice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "simpleTest";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        submit1();
+        submit2();
+        submit3();
+    }
+
+    private void submit1() {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        Future<Integer> future = executorService.submit(new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                int sum = 0;
+                for(int i = 0; i< 10; i++){
+                    sum += i;
+                }
+                return sum;
+            }
+        });
+        try {
+            Log.d(TAG, "the result of submit1 is: " + future.get());
+        }catch (InterruptedException | ExecutionException ex){
+            ex.printStackTrace();
+        }
+        executorService.shutdown();
+    }
+
+    private void submit2(){
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        Future<String> future = executorService.submit(new Runnable() {
+            @Override
+            public void run() {
+                Log.d(TAG, "do something");
+            }
+        }, "jiatai happy");
+        try {
+            Log.d(TAG, "the result of submit2 is: " + future.get());
+        }catch (InterruptedException | ExecutionException ex){
+            ex.printStackTrace();
+        }
+        executorService.shutdown();
+    }
+
+    private void submit3(){
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        Future<?> future = executorService.submit(new Runnable() {
+            @Override
+            public void run() {
+                Log.d(TAG, "do something");
+            }
+        });
+        try {
+            Log.d(TAG, "the result of submit3 is: " + future.get());
+        }catch (InterruptedException | ExecutionException ex){
+            ex.printStackTrace();
+        }
+        executorService.shutdown();
     }
 }
