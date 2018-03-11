e184373611645148f83872506d9f6ab363bbec8b
happyjiatai <835353929@qq.com>
2018-03-11 11:58:29 +0800

    demo_10_ExecutorService shutdown demo

--- demo_10_executorservice/src/main/java/com/example/demo_10_executorservice/MainActivity.java
+++ demo_10_executorservice/src/main/java/com/example/demo_10_executorservice/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.demo_10_executorservice;
 
+import android.renderscript.ScriptGroup;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,9 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        submit1();
-        submit2();
-        submit3();
+        shutdown1();
     }
 
     private void submit1() {
@@ -71,5 +70,94 @@ public class MainActivity extends AppCompatActivity {
             ex.printStackTrace();
         }
         executorService.shutdown();
+    }
+
+    private void shutdown1(){
+        Log.d(TAG, "----shutdown1 begin---- ");
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        Log.d(TAG, "----task1 begin---- ");
+        Future future1 = executorService.submit(new Task1());
+
+        Log.d(TAG, "----task2 begin---- ");
+        Future future2 = executorService.submit(new Task2());
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Log.d(TAG, "----task3 begin---- ");
+        Future future3 = executorService.submit(new Task3());
+
+        executorService.shutdown();
+        Log.d(TAG, "----executorService.shutdown---- ");
+
+        Future future4 = null;
+        try {
+            future4 = executorService.submit(new Task4());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+        }
+        try {
+            Log.d(TAG, "the result of shutdown1 is: " + future1.get());
+            Log.d(TAG, "the result of shutdown1 is: " + future2.get());
+            Log.d(TAG, "the result of shutdown1 is: " + future3.get());
+            Log.d(TAG, "the result of shutdown1 is: " + future4.get());
+        }catch (InterruptedException | ExecutionException ex){
+            ex.printStackTrace();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+        Log.d(TAG, "----shutdown1 end---- ");
+    }
+
+    private class Task1 implements Callable<String>{
+        @Override
+        public String call() throws Exception {
+            int sum = 0;
+            for(int i = 0; i< 10; i++){
+                sum += i;
+            }
+            return "task1";
+        }
+    }
+
+    private class Task2 implements Callable<String>{
+        @Override
+        public String call() throws Exception {
+            int sum = 0;
+            for(int i = 0; i< 10; i++){
+                sum += i;
+            }
+            Thread.sleep(20000);
+            return "task2";
+        }
+    }
+
+    private class Task3 implements Callable<String>{
+        @Override
+        public String call() throws Exception {
+            int sum = 0;
+            for(int i = 0; i< 10; i++){
+                sum += i;
+            }
+            Thread.sleep(2000);
+            return "task3";
+        }
+    }
+
+    private class Task4 implements Callable<String>{
+        @Override
+        public String call() throws Exception {
+            int sum = 0;
+            for(int i = 0; i< 10; i++){
+                sum += i;
+            }
+            Thread.sleep(2000);
+            return "task4";
+        }
     }
 }
