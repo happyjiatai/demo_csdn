b9e8888e40e965de839e43310c2ae25fe7740ebf
happyjiatai <835353929@qq.com>
2018-03-23 19:52:29 +0800

    ID:demo_13_runnable
    DEMO:upload runnable test demo

--- demo_13_runnable_levelUp/src/main/java/com/example/demo_13_runnable_levelup/RunnableTest.java
+++ demo_13_runnable_levelUp/src/main/java/com/example/demo_13_runnable_levelup/RunnableTest.java
@@ -1,14 +1,16 @@
 package com.example.demo_13_runnable_levelup;
 
 public class RunnableTest {
-    public static void main(String args[]){
+    public static void main(String args[]) throws InterruptedException {
         Banji banji = new Banji();
         Thread banZhuRen = new Thread(banji);
         banZhuRen.start();
+        Thread.sleep(200);
         System.out.println("the number of banji is :" + banji.toString());
 
         Thread fubanZhuRen = new Thread(banji);
         fubanZhuRen.start();
+        Thread.sleep(200);
         System.out.println("the number of banji is :" + banji.toString());
     }
 }
