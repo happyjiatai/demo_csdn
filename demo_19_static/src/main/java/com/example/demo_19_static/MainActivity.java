f58719dcb2de482e0e969a3305d4ebd73ae1c0f2
happyjiatai <835353929@qq.com>
2018-03-29 20:18:46 +0800

    ID:demo_19_static
    DEMO:upload static test demo

---
+++ demo_19_static/src/main/java/com/example/demo_19_static/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.demo_19_static;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.utils.StaticModle;
+
+public class MainActivity extends AppCompatActivity {
+    private static String TAG = "staticTest1";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, TAG + " onCreate");
+        final TextView tx = findViewById(R.id.textView);
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tx.setText("the number is: " + ++StaticModle.InnerClass.staticNumOfInnerClass);
+                Log.d(TAG, "the StaticModle.InnerClass.staticNumOfInnerClass num is: " + StaticModle.InnerClass.staticNumOfInnerClass);
+            }
+        });
+        Button btn2 =  findViewById(R.id.button2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClassName("com.example.demo_19_static","com.example.demo_19_static.MainActivity2");
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, TAG + " onDestroy");
+    }
+}
